68850d52a744294989fd890c339ba94487b5be1f
holic-x <1301996554@qq.com>
2024-12-31 16:01:28 +0800

    feat：1.两数之和
    feat：1.两数之和

---
+++ code/algorithm-demo/src/com/noob/algorithm/leetcode/q001/Main.java
@@ -0,0 +1,13 @@
+package com.noob.algorithm.leetcode.q001;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution1 solution1 = new Solution1();
+        Solution2 solution2 = new Solution2();
+
+        int[] nums = new int[]{};
+        solution1.twoSum(nums,3);
+        solution2.twoSum(nums,3);
+    }
+}
---
+++ code/algorithm-demo/src/com/noob/algorithm/leetcode/q001/Solution1.java
@@ -0,0 +1,23 @@
+package com.noob.algorithm.leetcode.q001;
+
+/**
+ * 🟢 1.两数之和
+ */
+public class Solution1 {
+
+    /**
+     * 思路1：双层循环
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len - 1; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        // 没有匹配到满足的条件
+        return new int[0];
+    }
+}
