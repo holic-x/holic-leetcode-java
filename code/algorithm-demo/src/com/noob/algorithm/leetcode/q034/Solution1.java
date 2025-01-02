ee7f50bcff02d9fbb195f89943c3211aae549c4e
holic-x <1301996554@qq.com>
2025-01-02 17:13:15 +0800

    feat：034.在排序数组中查找元素的第一个和最后一个位置
    feat：034.在排序数组中查找元素的第一个和最后一个位置

--- code/algorithm-demo/src/com/noob/algorithm/leetcode/q034/Solution1.java
+++ code/algorithm-demo/src/com/noob/algorithm/leetcode/q034/Solution1.java
@@ -1,19 +1,19 @@
-package com.noob.algorithm.leetcode.q34;
+package com.noob.algorithm.leetcode.q034;
 
 /**
- * 34.在排序数组中查找元素的第一个和最后一个位置
+ * 🟡 034.在排序数组中查找元素的第一个和最后一个位置 - https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/
  */
-public class Solution {
+public class Solution1 {
 
     // 暴力法：循环遍历数组，一次进行校验
     public int[] searchRange(int[] nums, int target) {
         // 定义结果集(开始索引,结束索引)
-        int[] res = {-1,-1};
+        int[] res = {-1, -1};
         // 定义target在数组中的匹配个数(计数器)
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target) {
-                if(count == 0){
+                if (count == 0) {
                     // 表示第一个检索到的匹配元素，将其加入结果集
                     res[0] = i;
                 }
@@ -21,8 +21,8 @@ public class Solution {
             }
         }
         // 最终封装结果集并返回
-        if(res[0] != -1){
-            res[1] = res[0] + count -1;
+        if (res[0] != -1) {
+            res[1] = res[0] + count - 1;
         }
         return res;
     }
