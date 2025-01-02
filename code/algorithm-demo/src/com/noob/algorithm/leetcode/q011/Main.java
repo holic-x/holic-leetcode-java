d7158f17a2cfb40e38ff79510b03ce3657288e35
holic-x <1301996554@qq.com>
2025-01-02 10:45:18 +0800

    feat：11.盛最多水的容器（双指针）
    feat：11.盛最多水的容器（双指针）

---
+++ code/algorithm-demo/src/com/noob/algorithm/leetcode/q011/Main.java
@@ -0,0 +1,9 @@
+package com.noob.algorithm.leetcode.q011;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution1 s = new Solution1();
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(s.maxArea(height));
+    }
+}
--- code/algorithm-demo/src/com/noob/algorithm/leetcode/q011/Solution1.java
+++ code/algorithm-demo/src/com/noob/algorithm/leetcode/q011/Solution1.java
@@ -1,27 +1,23 @@
-package com.noob.algorithm.leetcode.q11;
+package com.noob.algorithm.leetcode.q011;
 
 /**
- * 11.盛最多水的容器（双指针）
+ * 🟡 011 盛最多水的容器（双指针）
+ * https://leetcode.cn/problems/container-with-most-water/description/
  */
-public class Solution {
+public class Solution1 {
 
+    // 思路：双指针，移动短板、更新最大面积
     public int maxArea(int[] height) {
         // 双指针：定义左侧指针、右侧指针、盛水面积
         int i = 0, j = height.length - 1, res = 0;
         // 当两个指针相遇则停止，在比较过程中计算面积大小（宽度一定的情况下，面积大小取决于短板高度）
-        while(i < j) {
+        while (i < j) {
             // 比较当前指针对应的柱高度，移动短板一侧，获取较大的res
             res = height[i] < height[j] ?
-                    Math.max(res, (j - i) * height[i++]):
+                    Math.max(res, (j - i) * height[i++]) :
                     Math.max(res, (j - i) * height[j--]);
         }
         return res;
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        int[] height = {1,8,6,2,5,4,8,3,7};
-        System.out.println(s.maxArea(height));
     }
 
 }
