1fc1720d4a7e99cea6ddb2d430d86c02c0140d32
holic-x <1301996554@qq.com>
2025-01-02 13:29:00 +0800

    refactor
    refactor

---
+++ code/algorithm-demo/src/com/noob/algorithm/base/template/Main.java
@@ -0,0 +1,29 @@
+package com.noob.algorithm.base.template;
+
+public class Main {
+
+    public void initCase(){
+
+    }
+
+    public static void testCase1() {
+
+
+    }
+
+
+    public static void testCase2() {
+
+    }
+
+
+    public static void main(String[] args) {
+
+        testCase1();
+
+        System.out.println("-------------------------------");
+
+        testCase2();
+
+    }
+}
