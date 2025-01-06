45b23232724f40650df00cc0e852b66107a0f09a
holic-x <1301996554@qq.com>
2025-01-06 15:45:00 +0800

    feat：121.买卖股票的最佳时机、122.买卖股票的最佳时机II
    feat：121.买卖股票的最佳时机、122.买卖股票的最佳时机II

---
+++ code/algorithm-demo/src/com/noob/algorithm/leetcode/q122/Main.java
@@ -0,0 +1,57 @@
+package com.noob.algorithm.leetcode.q122;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    // 初始化测试用例
+    public static List<int[]> initCase() {
+        List<int[]> caseList = new ArrayList<>();
+
+        // todo 补充测试用例集合
+        caseList.add(new int[]{7, 1, 5, 3, 6, 4}); // 7
+        caseList.add(new int[]{1, 2, 3, 4, 5}); // 4
+        caseList.add(new int[]{7, 6, 4, 3, 1}); // 0
+
+        return caseList;
+    }
+
+    public static void testSolution1(List<int[]> caseList) {
+
+        for (int[] prices : caseList) {
+            // ① 调用算法
+            Solution1 solution = new Solution1();
+            int res = solution.maxProfit(prices);
+
+            // ② 打印数据
+            System.out.println(res);
+
+            // 分隔符
+            System.out.println("********************");
+
+        }
+
+    }
+
+
+    public static void testSolution2(List<int[]> caseList) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+        // 参数初始化
+        List<int[]> caseList = initCase();
+
+        System.out.println("---------- 算法①调用测试 ----------");
+
+        testSolution1(caseList);
+
+        System.out.println("---------- 算法②调用测试 ----------");
+
+        testSolution2(caseList);
+
+    }
+}
