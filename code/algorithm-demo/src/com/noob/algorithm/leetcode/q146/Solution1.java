f0d8cbaf41fc440ff450543a2df7d49b90c30db5
holic-x <1301996554@qq.com>
2025-01-06 16:38:08 +0800

    feat：146-LRU缓存
    feat：146-LRU缓存

--- code/algorithm-demo/src/com/noob/algorithm/leetcode/q146/Solution1.java
+++ code/algorithm-demo/src/com/noob/algorithm/leetcode/q146/Solution1.java
@@ -3,16 +3,16 @@ package com.noob.algorithm.leetcode.q146;
 import java.util.*;
 
 /**
- * 146-LRU缓存
+ * 🟡 146-LRU缓存 - https://leetcode.cn/problems/lru-cache/description/
  * 基于LinkedHashMap实现:存储key、value
  */
 class LRUCache {
 
     // 定义缓存容量阈值
-    int capacity ;
+    int capacity;
 
     // 定义数据结构（存储集合）
-    LinkedHashMap <Integer, Integer> cache;
+    LinkedHashMap<Integer, Integer> cache;
 
     // 构造函数
     public LRUCache(int capacity) {
@@ -23,7 +23,7 @@ class LRUCache {
     // 访问元素（判断元素是否存在，更新访问节点位置（重新插入：即先删后插））
     public int get(int key) {
         // 如果指定值存在
-        if(cache.containsKey(key)) {
+        if (cache.containsKey(key)) {
             // 记录当前值
             int value = cache.get(key);
             // 移除节点
@@ -31,20 +31,20 @@ class LRUCache {
             // 重新插入节点到链表尾部
             cache.put(key, value);
             return value;
-        }else{
+        } else {
             return -1;
         }
     }
 
     // 插入元素（判断插入数据是否存在，存储则直接覆盖，如果不存在则继续判断是否超出阈值，超出阈值则需要触发LRU淘汰机制）
     public void put(int key, int value) {
-        if(cache.containsKey(key)) {
+        if (cache.containsKey(key)) {
             // key已存在则覆盖（先删后加）
             cache.remove(key);
             cache.put(key, value);
-        }else{
+        } else {
             // 校验阈值
-            if(cache.size()>=capacity){
+            if (cache.size() >= capacity) {
                 // 超出阈值触发清理(因为LinkedHashMap是有序的，所以此处通过迭代器获取到第一个元素，然后直接移除)
                 Iterator<Integer> iterator = cache.keySet().iterator();
                 cache.remove(iterator.next());
@@ -53,16 +53,52 @@ class LRUCache {
             cache.put(key, value);
         }
     }
+
+    // 打印当前缓存数据
+    public void print() {
+        Set<Map.Entry<Integer, Integer>> entrySet = cache.entrySet();
+        for (Map.Entry<Integer, Integer> item : entrySet) {
+            System.out.println(item.getKey() + "=" + item.getValue());
+        }
+    }
 }
 
+
 /**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
+ * 自定义LRUCache
  */
+public class Solution1 {
 
-
-public class Solution1{
-
+    public static void main(String[] args) {
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.print();
+        System.out.println("******************************");
+        lRUCache.put(1, 1); // 缓存是 {1=1}
+        lRUCache.print();
+        System.out.println("******************************");
+        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+        lRUCache.print();
+        System.out.println("******************************");
+        lRUCache.get(1);    // 返回 1
+        lRUCache.print();
+        System.out.println("******************************");
+        lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        lRUCache.print();
+        System.out.println("******************************");
+        lRUCache.get(2);    // 返回 -1 (未找到)
+        lRUCache.print();
+        System.out.println("******************************");
+        lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+        lRUCache.print();
+        System.out.println("******************************");
+        lRUCache.get(1);    // 返回 -1 (未找到)
+        lRUCache.print();
+        System.out.println("******************************");
+        lRUCache.get(3);    // 返回 3
+        lRUCache.print();
+        System.out.println("******************************");
+        lRUCache.get(4);    // 返回 4
+        lRUCache.print();
+        System.out.println("******************************");
+    }
 }
