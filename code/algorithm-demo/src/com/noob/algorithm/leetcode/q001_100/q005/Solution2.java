471e6d174d2b153ceeaa6443357e5d4656e2accc
holic-x <1301996554@qq.com>
2025-01-07 11:16:30 +0800

    feat：005 最长回文子串
    feat：005 最长回文子串

---
+++ code/algorithm-demo/src/com/noob/algorithm/leetcode/q001_100/q005/Solution2.java
@@ -0,0 +1,18 @@
+package com.noob.algorithm.leetcode.q001_100.q005;
+
+/**
+ * 🟡 005 最长回文子串 - https://leetcode.cn/problems/longest-palindromic-substring/
+ */
+public class Solution2 {
+
+    /**
+     * 多维动态规划思路：todo
+     */
+    public String longestPalindrome(String s) {
+        String maxHuiwenStr = "";
+
+        // 返回结果
+        return maxHuiwenStr;
+    }
+
+}
