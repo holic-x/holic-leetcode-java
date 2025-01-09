88ec2619db2471c07c09a98a7403effa20ad4b1a
holic-x <1301996554@qq.com>
2025-01-09 17:48:46 +0800

    feat：算法题解补充
    feat：算法题解补充

---
+++ code/algorithm-demo/src/com/noob/algorithm/leetcode/q101_200/q148/Solution2.java
@@ -0,0 +1,21 @@
+package com.noob.algorithm.leetcode.q101_200.q148;
+
+import com.noob.algorithm.base.dataStructure.linkedList.ListNode;
+
+/**
+ * 🟡 148-单链表排序 - https://leetcode.cn/problems/sort-list/
+ */
+public class Solution2 {
+
+
+    /**
+     * 归并排序思路：
+     * ① 找中点、拆分两个链表
+     * ② 分：分别对两个链表进行排序
+     * ③ 合：将排序后的两个链表进行合并（参考【合并两个有序链表思路】）
+     */
+    public ListNode sortList(ListNode head) {
+
+
+    }
+}
